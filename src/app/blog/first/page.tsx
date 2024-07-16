e9c256be99d079a56a74039051b7abfb1c3ee74e
Mohammad Ashif <mohammadashif510@gmail.com>
2024-07-16 12:42:37 +0600

    nested routing added

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,6 @@
+const First = () => {
+    return (
+    <h1>this is first post</h1>
+    );
+};
+export default First;
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+const Blog = () => {
+    return (
+        <h1>This is Blog page</h1>
+    )
+}
+
+
+export default Blog;
